7ed39808fcb0d95331a00d86bea8993229b34eee
Fitri Nur Azis <email@contoh.com>
2024-09-08 06:39:04 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/uas-ekonomi-bisnis/*" element={<QuestionsPage />} />
+        <Route path="/*" element={<QuestionsPage />} />
       </Routes>
     </Router>
   );
